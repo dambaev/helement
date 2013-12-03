f2cd8baa16aa42b20206f799e07522b0fa098e4b
Dambaev Alexander <dambaev@dambaev-host.baikalbank.ru>
2013-12-03 22:17:23 +0900

    cabalizing

--- src/HElementary/ElementaryFFI_hsc.c
+++
@@ -1,3 +0,0 @@
-#include "ElementaryFFI_hsc.h"
-#line 30 "ElementaryFFI.hsc"
-elm_object_text_set , Ptr <Evas_Object> -> Ptr CChar -> IO ()
--- src/HElementary/Elementary_stub.h
+++
@@ -1,9 +0,0 @@
-#include "HsFFI.h"
-#ifdef __cplusplus
-extern "C" {
-#endif
-extern void HElementaryziElementary_d1wB(StgStablePtr the_stableptr, HsPtr a1, HsPtr a2, HsPtr a3);
-#ifdef __cplusplus
-}
-#endif
-
